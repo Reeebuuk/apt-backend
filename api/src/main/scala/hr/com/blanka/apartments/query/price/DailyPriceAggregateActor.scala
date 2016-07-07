f25f161e5a88b25f473f6a3eb4f81cb7b544ad4e
kuzelac <raven1>
2016-07-07 19:13:44 +0100

    simplified price aggregation
    Former-commit-id: 5852d649cccfefeedc804ef483d729f930136753

--- api/src/main/scala/hr/com/blanka/apartments/query/price/DailyPriceAggregateActor.scala
+++ api/src/main/scala/hr/com/blanka/apartments/query/price/DailyPriceAggregateActor.scala
@@ -8,49 +8,38 @@ object DailyPriceAggregateActor {
   def apply() = Props(classOf[DailyPriceAggregateActor])
 
   val extractEntityId: ShardRegion.ExtractEntityId = {
-    case e @ DailyPriceSaved(userId, unitId, _, _, _) => (s"$userId$unitId", e)
-    case e @ LookupPriceForDay(userId, unitId, _) => (s"$userId$unitId", e)
+    case e@DailyPriceSaved(userId, unitId, _, _, _) => (s"$userId$unitId", e)
+    case e@LookupPriceForDay(userId, unitId, _) => (s"$userId$unitId", e)
   }
 
   val extractShardId: ShardRegion.ExtractShardId = {
-    case e @ DailyPriceSaved(userId, unitId, _, _, _) => s"$userId$unitId"
-    case e @ LookupPriceForDay(userId, unitId, _) => s"$userId$unitId"
+    case e@DailyPriceSaved(userId, unitId, _, _, _) => s"$userId$unitId"
+    case e@LookupPriceForDay(userId, unitId, _) => s"$userId$unitId"
   }
 }
 
 class DailyPriceAggregateActor extends Actor {
 
-  def updateState(newDailyPrice: DailyPriceSaved, currentDailyPrices: Map[String, Map[Int, Map[DayMonth, Double]]]): Unit = {
+  def updateState(newDailyPrice: DailyPriceSaved, currentDailyPrices: Map[DayMonth, List[Double]]): Unit = {
 
-    val newPrices : Map[String, Map[Int, Map[DayMonth, Double]]] = currentDailyPrices.get(newDailyPrice.userId) match {
-      case None => currentDailyPrices + (newDailyPrice.userId -> Map(newDailyPrice.unitId -> Map(newDailyPrice.dayMonth -> newDailyPrice.price)))
-      case Some(currentPricesForUser) => currentPricesForUser.get(newDailyPrice.unitId) match {
-        case None => currentDailyPrices + (newDailyPrice.userId -> (currentPricesForUser ++ Map(newDailyPrice.unitId -> Map(newDailyPrice.dayMonth -> newDailyPrice.price))))
-        case Some(currentPriceForUnit) =>
-          currentDailyPrices + (newDailyPrice.userId -> (currentPricesForUser + (newDailyPrice.unitId -> (currentPriceForUnit + (newDailyPrice.dayMonth -> newDailyPrice.price)))))
-      }
+    val newPrices: List[Double] = currentDailyPrices.get(newDailyPrice.dayMonth) match {
+      case Some(priceForDay) => newDailyPrice.price :: priceForDay
+      case None => List(newDailyPrice.price)
     }
-    context become active(newPrices)
+
+    context become active(currentDailyPrices + (newDailyPrice.dayMonth -> newPrices))
   }
 
-  override def receive = active(Map[String, Map[Int, Map[DayMonth, Double]]]())
+  override def receive = active(Map[DayMonth, List[Double]]())
 
-  def active(currentDailyPrices: Map[String, Map[Int, Map[DayMonth, Double]]]): Receive = {
-    case e : DailyPriceSaved =>
+  def active(currentDailyPrices: Map[DayMonth, List[Double]]): Receive = {
+    case e: DailyPriceSaved =>
       updateState(e, currentDailyPrices)
 
     case LookupPriceForDay(userId, unitId, day) =>
-      val lastPrice: Double = currentDailyPrices.get(userId) match {
+      val lastPrice: Double = currentDailyPrices.get(day) match {
         case None => 0
-        case Some(priceForUnit) => priceForUnit.get(unitId) match {
-          case None => 0
-          case Some(priceForDay) =>
-            priceForDay.get(day) match {
-              case None => 0
-              case Some(price) => price
-            }
-        }
-
+        case Some(price) => price.head
       }
 
       sender() ! PriceDayFetched(lastPrice)
