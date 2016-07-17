0abfd61f8dbe2df5a3e809be49acf8e3011ce78a
kuzelac <raven1>
2016-07-17 13:51:56 +0100

    test working when named PriceServiceTest but not PriceTest, who's crazy here?
    Former-commit-id: 9d1fa5473caf371461d588f81e0196e7a7497dff [formerly be9a4f551b181d5ed43adae58fcfc4808f56b1fb]
    Former-commit-id: 95700fe5763db8507bae35bfe51a965e7331921b

--- api/src/test/scala/hr/com/blanka/apartments/PriceServiceTest.scala
+++ api/src/test/scala/hr/com/blanka/apartments/PriceServiceTest.scala
@@ -22,7 +22,7 @@ import spray.json._
 import scala.concurrent.duration._
 import scala.language.implicitConversions
 
-class PriceTest extends IntegrationTestMongoDbSupport with Matchers with ScalatestRouteTest with Eventually {
+class PriceServiceTest extends IntegrationTestMongoDbSupport with Matchers with ScalatestRouteTest with Eventually {
 
   import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
   protected val log: LoggingAdapter = NoLogging
