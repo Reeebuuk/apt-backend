37b7a3e27cc1f2e7df39a3bba9974be45299d739
kuzelac <raven1>
2016-08-01 15:12:10 +0200

    first unit test working
    Former-commit-id: 239c2ad2855e07f8c45e58469c2c21a3d83efd8e

--- api/src/test/scala/hr/com/blanka/apartments/command/price/CommandPriceActorTest.scala
+++ api/src/test/scala/hr/com/blanka/apartments/command/price/CommandPriceActorTest.scala
@@ -1,24 +1,31 @@
 package hr.com.blanka.apartments.command.price
 
-import akka.actor.ActorSystem
-import akka.testkit.TestKit
-import org.joda.time.DateTime
-import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
-import akka.pattern.ask
+import akka.actor.{Actor, ActorRefFactory, ActorSystem, Props}
+import akka.testkit.{TestActorRef, TestKit, TestProbe}
 import akka.util.Timeout
-import org.scalactic
+import com.typesafe.config.ConfigFactory
+import org.joda.time.DateTime
 import org.scalactic.Good
-import org.scalatest.concurrent.{Eventually, IntegrationPatience}
+import org.scalatest.concurrent.Eventually
+import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
 
-import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 import scala.language.postfixOps
-import scala.util.Success
 
-class CommandPriceActorTest extends TestKit(ActorSystem("test-benefits")) with FlatSpecLike with Matchers
+class CommandPriceActorTest extends TestKit(ActorSystem("test-benefits",
+  ConfigFactory.parseString("""akka.loggers = ["akka.testkit.TestEventListener"] """))) with FlatSpecLike with Matchers
   with BeforeAndAfterAll with Eventually{
 
-  val commandPriceActor = system.actorOf(CommandPriceActor())
+  class MockedChild extends Actor {
+    def receive = {
+      case SavePriceForSingleDay(userId, unitId, day, price) =>
+         sender() ! DailyPriceSaved(userId, unitId, day, price, new DateTime())
+    }
+  }
+
+  val probe = TestProbe()
+  val mockProps = Props(classOf[MockedChild], this)
+  val commandPriceActor = TestActorRef(new CommandPriceActor(mockProps))
 
   implicit val timeout = Timeout(3 seconds)
 
@@ -34,10 +41,9 @@ class CommandPriceActorTest extends TestKit(ActorSystem("test-benefits")) with F
     val firstTo = firstFrom.plusDays(5)
     val firstPrice = SavePriceRange(userId, unitId, firstFrom.getMillis, firstTo.getMillis, 35)
 
+    probe.send(commandPriceActor, firstPrice)
     eventually {
-      val futureResult = commandPriceActor ? firstPrice
-      val Success(result: Int) = futureResult.value.get
-      result should be(Good)
+      probe.expectMsg(Good)
     }
   }
 }
