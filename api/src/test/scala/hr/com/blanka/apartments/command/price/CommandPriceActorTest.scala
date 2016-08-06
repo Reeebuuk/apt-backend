f0247e83dd53f8dd3976d8dcf77ad5a78b4c6237
kuzelac <raven1>
2016-08-06 12:07:27 +0200

    Enhanced command price actor unit tests
    Former-commit-id: 78d739bf2316a9a9925ecd1cc5828466591487e4 [formerly aa7325ab3ed6bd85acbd5c792c471f3fa4390aa2]
    Former-commit-id: 61f43140a9700078100f27a10aa114a305fef3c2

--- api/src/test/scala/hr/com/blanka/apartments/command/price/CommandPriceActorTest.scala
+++ api/src/test/scala/hr/com/blanka/apartments/command/price/CommandPriceActorTest.scala
@@ -4,46 +4,78 @@ import akka.actor.{Actor, ActorSystem, Props}
 import akka.testkit.{TestActorRef, TestKit, TestProbe}
 import akka.util.Timeout
 import com.typesafe.config.ConfigFactory
+import hr.com.blanka.apartments.validation.ErrorMessages._
 import org.joda.time.{DateTime, LocalDate}
-import org.scalactic.Good
-import org.scalatest.concurrent.Eventually
-import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
+import org.scalactic.{Bad, Good}
+import org.scalatest.{BeforeAndAfterAll, FreeSpecLike, Matchers}
 
 import scala.concurrent.duration._
 import scala.language.postfixOps
 
 class CommandPriceActorTest extends TestKit(ActorSystem("test-benefits",
-  ConfigFactory.parseString("""akka.loggers = ["akka.testkit.TestEventListener"] """))) with FlatSpecLike with Matchers
-  with BeforeAndAfterAll with Eventually{
-
-  class MockedChild extends Actor {
-    def receive = {
-      case SavePriceForSingleDay(userId, unitId, day, price) =>
-         sender() ! DailyPriceSaved(userId, unitId, day, price, new DateTime())
-    }
-  }
+  ConfigFactory.parseString("""akka.loggers = ["akka.testkit.TestEventListener"] """))) with FreeSpecLike with Matchers
+  with BeforeAndAfterAll {
 
   val probe = TestProbe()
-  val mockProps = Props(classOf[MockedChild], this)
-  val commandPriceActor = TestActorRef(new CommandPriceActor(mockProps))
 
-  implicit val timeout = Timeout(3 seconds)
+  implicit val timeout = Timeout(1 seconds)
 
   override def afterAll {
     TestKit.shutdownActorSystem(system)
   }
 
-  "SavePriceRange event" should "return Good result if save range is valid" in {
+  class MockPricePersistActor extends Actor {
+    def receive = {
+      case SavePriceForSingleDay("notSaving", unitId, day, price) =>
+
+      case SavePriceForSingleDay(userId, unitId, day, price) =>
+        sender() ! DailyPriceSaved(userId, unitId, day, price, new DateTime())
+    }
+  }
+
+  val mockProps = Props(classOf[MockPricePersistActor], this)
+  val commandPriceActor = TestActorRef(new CommandPriceActor(mockProps){
+    override val timeout = Timeout(100 milliseconds)
+  })
+
+  "SavePriceRange" - {
     val userId = "user"
     val unitId = 1
+    "return Good result if save range is valid" in {
+      val from = new LocalDate()
+      val to = from.plusDays(5)
+      val savePriceRange = SavePriceRange(userId, unitId, from, to, 35)
 
-    val firstFrom = new LocalDate()
-    val firstTo = firstFrom.plusDays(5)
-    val firstPrice = SavePriceRange(userId, unitId, firstFrom, firstTo, 35)
-
-    probe.send(commandPriceActor, firstPrice)
-    eventually {
+      probe.send(commandPriceActor, savePriceRange)
       probe.expectMsg(Good)
+    }
+
+    "return Bad result if range is not valid" in {
+      val from = new LocalDate()
+      val to = from.plusDays(5)
+      val savePriceRange = SavePriceRange(userId, unitId, to, from, 35)
+
+      probe.send(commandPriceActor, savePriceRange)
+      probe.expectMsg(Bad(toDateBeforeFromDateErrorMessage(to, from)))
+    }
+
+    "return Bad result if range dates are in the past" in {
+      val from = new LocalDate().minusDays(10)
+      val to = from.plusDays(5)
+      val savePriceRange = SavePriceRange(userId, unitId, from, to, 35)
+
+      probe.send(commandPriceActor, savePriceRange)
+      probe.expectMsg(Bad(List(dateIsInPastErrorMessage("From", from), dateIsInPastErrorMessage("To", to)).mkString(", ")))
+    }
+
+    "return Bad result if whole range is not successfully saved" in {
+      val userId = "notSaving"
+      val from = new LocalDate()
+      val to = from.plusDays(5)
+      val savePriceRange = SavePriceRange(userId, unitId, from, to, 35)
+
+      probe.send(commandPriceActor, savePriceRange)
+      probe.expectMsg(Bad(persistingDailyPricesErrorMessage))
     }
   }
 }
