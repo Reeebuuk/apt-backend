94146e7348dfd159419b0411164948f31fbf8cf0
kuzelac <raven1>
2016-08-07 17:45:36 +0200

    refactoring common code
    Former-commit-id: 4e8694dac383d1ccfa5a013b4e7da53df80dc5f0

--- api/src/test/scala/hr/com/blanka/apartments/validation/BasicValidationTest.scala
+++ api/src/test/scala/hr/com/blanka/apartments/validation/BasicValidationTest.scala
@@ -8,33 +8,33 @@ class BasicValidationTest extends WordSpec with Matchers{
 
   import ErrorMessages._
 
-  "validateAndGetDurationInDays" when {
+  "validateDuration" when {
     "from date is before to date" should {
       "return bad result with error message" in {
         val to = new LocalDate()
         val from = to.plusDays(1)
-        BasicValidation.validateAndGetDurationInDays(from, to) should be (Bad(One(toDateBeforeFromDateErrorMessage(from, to))))
+        BasicValidation.validateDuration(from, to) should be (Bad(One(toDateBeforeFromDateErrorMessage(from, to))))
       }
     }
     "from date is in the past" should {
       "return bad result with error message" in {
         val from = new LocalDate().minusDays(1)
         val to = from.plusDays(3)
-        BasicValidation.validateAndGetDurationInDays(from, to) should be (Bad(One(dateIsInPastErrorMessage("From", from))))
+        BasicValidation.validateDuration(from, to) should be (Bad(One(dateIsInPastErrorMessage("From", from))))
       }
     }
     "both dates are in the past" should {
       "return bad result with two error messages" in {
         val from = new LocalDate().minusDays(2)
         val to = new LocalDate().minusDays(1)
-        BasicValidation.validateAndGetDurationInDays(from, to) should be (Bad(Many(dateIsInPastErrorMessage("From", from), dateIsInPastErrorMessage("To", to))))
+        BasicValidation.validateDuration(from, to) should be (Bad(Many(dateIsInPastErrorMessage("From", from), dateIsInPastErrorMessage("To", to))))
       }
     }
     "valid date range is passed in it" should {
       "return the duration between from and to in days" in {
         val from = new LocalDate()
         val to = from.plusDays(5)
-        BasicValidation.validateAndGetDurationInDays(from, to) should be (Good(5))
+        BasicValidation.validateDuration(from, to) should be (Good(5))
       }
     }
   }
